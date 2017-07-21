039896fd02a484d23fa205290435b8ea2ba5d6bb
Matt Hicks <matt@outr.com>
2017-07-21 14:55:19 -0500

    Fully functioning CrossApplication.

--- build.sbt
+++ build.sbt
@@ -4,3 +4,4 @@ version := "1.0.0-SNAPSHOT"
 sbtPlugin := true
 scalaVersion := "2.10.6"
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.18")
+libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
--- src/main/scala/io/youi/plugin/YouIPlugin.scala
+++ src/main/scala/io/youi/plugin/YouIPlugin.scala
@@ -6,31 +6,68 @@ import org.scalajs.sbtplugin.ScalaJSPlugin
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import org.scalajs.sbtplugin.cross.CrossProject
 
+import scala.language.experimental.macros
+import scala.reflect.macros.Context
+
 import scala.language.implicitConversions
 
 object YouIPlugin extends AutoPlugin {
-  def crossApplication(name: String): PartialCrossApplication = new PartialCrossApplication(name)
+  object autoImport {
+    def crossApplication: PartialCrossApplication = macro CrossApplication.partial
 
-  implicit def crossApplicationDependency(ca: CrossApplication): CrossApplicationDependencies = {
-    CrossApplicationDependencies(ClasspathDependency(ca.js, None), ClasspathDependency(ca.jvm, None))
-  }
+    implicit def crossApplicationDependency(ca: CrossApplication): CrossApplicationDependencies = {
+      CrossApplicationDependencies(ClasspathDependency(ca.js, None), ClasspathDependency(ca.jvm, None))
+    }
 
-  implicit def crossProjectDependency(cp: CrossProject): CrossApplicationDependencies = {
-    CrossApplicationDependencies(ClasspathDependency(cp.js, None), ClasspathDependency(cp.jvm, None))
+    implicit def crossProjectDependency(cp: CrossProject): CrossApplicationDependencies = {
+      CrossApplicationDependencies(ClasspathDependency(cp.js, None), ClasspathDependency(cp.jvm, None))
+    }
   }
 }
 
-class PartialCrossApplication(name: String) {
-  def in(dir: File): CrossApplication = CrossApplication(
-    js = Project(s"${name}JS", new File(dir, "js")),
-    jvm = Project(s"${name}JVM", new File(dir, "jvm"))
-  ).jsSettings(
-    artifactPath in Compile in fastOptJS := (baseDirectory.value / ".." / "jvm" / "src" / "main" / "resources" / "app" / "application-fastopt.js"),
-    artifactPath in Test in fastOptJS := (baseDirectory.value / ".." / "jvm" / "src" / "main" / "resources" / "app" / "application-fastopt.js"),
-    artifactPath in fullOptJS := (baseDirectory.value / ".." / "jvm" / "src" / "main" / "resources" / "app" / "application-opt.js"),
-    crossTarget in packageJSDependencies := baseDirectory.value / ".." / "jvm" / "src" / "main" / "resources" / "app",
-    crossTarget in packageMinifiedJSDependencies := baseDirectory.value / ".." / "jvm" / "src" / "main" / "resources" / "app"
-  ).enableJSPlugins(ScalaJSPlugin)
+class PartialCrossApplication(id: String) {
+  def in(dir: File): CrossApplication = {
+    val baseDir = dir.getAbsoluteFile
+    val sharedSource = baseDir / "shared" / "src"
+    val jvmSource = baseDir / "jvm" / "src"
+    val jvmResources = jvmSource / "main" / "resources"
+    val jvmApp = jvmResources / "app"
+    CrossApplication(
+      js = Project(s"${id}JS", new File(dir, "js")),
+      jvm = Project(s"${id}JVM", new File(dir, "jvm"))
+    ).settings(
+      // Add shared source
+      unmanagedSourceDirectories in Compile ++= {
+        makeCrossSources(Some(sharedSource / "main" / "scala"), scalaBinaryVersion.value, crossPaths.value)
+      },
+      unmanagedSourceDirectories in Test ++= {
+        makeCrossSources(Some(sharedSource / "test" / "scala"), scalaBinaryVersion.value, crossPaths.value)
+      }
+    ).jsSettings(
+      artifactPath in Compile in fastOptJS := jvmApp / "application-fastopt.js",
+      artifactPath in Test in fastOptJS := jvmApp / "application-fastopt.js",
+      artifactPath in Compile in fullOptJS := jvmApp / "application.js",
+      artifactPath in Test in fullOptJS := jvmApp / "application.js",
+      artifactPath in Compile in packageJSDependencies := jvmApp / "application-jsdeps.js",
+      artifactPath in Test in packageJSDependencies := jvmApp / "application-jsdeps.js",
+      artifactPath in Compile in packageMinifiedJSDependencies := jvmApp / "application-jsdeps.min.js",
+      artifactPath in Test in packageMinifiedJSDependencies := jvmApp / "application-jsdeps.min.js",
+      skip in packageJSDependencies := false
+    ).enableJSPlugins(ScalaJSPlugin)
+  }
+
+  // Inspired by sbt's Defaults.makeCrossSources
+  private def makeCrossSources(sharedSrcDir: Option[File],
+                               scalaBinaryVersion: String, cross: Boolean): Seq[File] = {
+    sharedSrcDir.fold[Seq[File]] {
+      Seq.empty
+    } { srcDir =>
+      if (cross)
+        Seq(srcDir.getParentFile / s"${srcDir.name}-$scalaBinaryVersion", srcDir)
+      else
+        Seq(srcDir)
+    }
+  }
 }
 
 case class CrossApplication(js: Project, jvm: Project) {
@@ -73,6 +110,47 @@ case class CrossApplication(js: Project, jvm: Project) {
   def jvmSettings(settings: Def.SettingsDefinition*): CrossApplication = {
     copy(jvm = jvm.settings(settings: _*))
   }
+}
+
+object CrossApplication {
+  def partial(c: Context): c.Expr[PartialCrossApplication] = {
+    import c.universe._
+
+    val enclosingValName = definingValName(c, methodName =>
+      s"""$methodName must be directly assigned to a val, such as `val x = $methodName`.""")
+    val name = c.Expr[String](Literal(Constant(enclosingValName)))
+    reify(new PartialCrossApplication(name.splice))
+  }
+
+  private def definingValName(c: Context, invalidEnclosingTree: String => String): String = {
+    import c.universe._
+    val methodName = c.macroApplication.symbol.name
+
+    // trim is not strictly correct, but macros don't expose the API necessary
+    def processName(n: Name): String = n.decoded.trim
+
+    def enclosingVal(trees: List[c.Tree]): String = trees match {
+      case vd @ ValDef(_, name, _, _) :: ts =>
+        processName(name)
+
+      case (_: Apply | _: Select | _: TypeApply) :: xs =>
+        enclosingVal(xs)
+
+      // lazy val x: X = <methodName> has this form for some reason
+      // (only when the explicit type is present, though)
+      case Block(_, _) :: DefDef(mods, name, _, _, _, _) :: xs if mods.hasFlag(Flag.LAZY) =>
+        processName(name)
+      case _ =>
+        c.error(c.enclosingPosition, invalidEnclosingTree(methodName.decoded))
+        "<error>"
+    }
+
+    enclosingVal(enclosingTrees(c).toList)
+  }
+
+  def enclosingTrees(c: Context): Seq[c.Tree] =
+    c.asInstanceOf[reflect.macros.runtime.Context].callsiteTyper.
+      context.enclosingContextChain.map(_.tree.asInstanceOf[c.Tree])
 }
 
 case class CrossApplicationDependencies(js: ClasspathDependency, jvm: ClasspathDependency)
